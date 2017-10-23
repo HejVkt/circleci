c5dcfdbcd5e8dc45a69ea49126eef851f1c2433c
Aleksander Semenov <hejvkt@gmail.com>
2017-10-24 02:41:13 +0300

    fix dusk error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -41,10 +41,5 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->isLocal()) {
             $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
         }
-
-        if ($this->app->environment('local', 'testing')) {
-            $this->app->register(DuskServiceProvider::class);
-        }
-        //
     }
 }
